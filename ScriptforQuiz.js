05d7ee758c45dc794166854ae5a9510be4e731ee
Elsabet Wuletaw <elsabetwuletaw@gmail.com>
2025-01-05 07:42:17 -0800

    chore: merge conflict resolved

---
+++ ScriptforQuiz.js
@@ -0,0 +1,254 @@
+const questions = [
+  {
+    question: " አንድ  ሰው  ያለውን  ሀሳብ  ለሌሎች  ከማካፈሉ  በፊት  ማድረግ  ያለበት ",
+    answer: [
+      { text: " ዋና  ዋና  ነጥቦችን መዝግቦ መያዝ      ", correct: false },
+      { text: " የግንኙነት  መዘዞችን መረዳት", correct: false },
+      { text: " ሰዎች እንዲያምኑህ ማድረግ", correct: false },
+      { text: "ሁሉም", correct: true },
+    ],
+  },
+  {
+    question: " አንድ  በትራንስፖርት  ዘርፍ  የተሰማራ  ግለሰብ  ከቦታ  ወደ  ቦታ  በሚንቀሳቀስበት ጊዜ የአከባቢውን ህብረተሰብ       ጠንቅቆ  መረዳት  ሊገጥመው  የሚችለውን  ችግር  ያቃልላል፡፡ ",
+    answer: [
+      { text: "ቋንቋ ", correct: false },
+      { text: " ሀይማኖት ", correct: false },
+      { text: ". ባህል ", correct: false },
+      { text: "ሁሉም መልስ ነው", correct: true },
+    ],
+  },
+  {
+    question: " በሁለት  አካላት  መካከል  የሚፈጠርን  ግጭት  ለማብረድ  መውሰድ ያለበት እርምጃ ያልሆነው የቱ ነው?  ",
+    answer: [
+      { text: " ሊያበሳጩ  የሚችሉ  ቃላትን  ከመሰንዘር  መቆጠብ ", correct: false },
+      { text: "  እራስን መቆጣጠር  ", correct: false },
+      { text: ". ከአካባቢው በመራቅ ", correct: false },
+      { text: "ሁሉም መልስ ነው", correct: true },
+    ],
+  },
+  {
+    question: " አንድ  በትራንስፖርት  ዘርፍ  የተሰማራ  ግለሰብ  ከቦታ  ወደ  ቦታ  በሚንቀሳቀስበት ጊዜ የአከባቢውን ህብረተሰብ       ጠንቅቆ  መረዳት  ሊገጥመው  የሚችለውን  ችግር  ያቃልላል፡፡ ",
+    answer: [
+      { text: "ቋንቋ ", correct: false },
+      { text: " ሀይማኖት ", correct: false },
+      { text: ". ባህል ", correct: false },
+      { text: "ሁሉም መልስ ነው", correct: true },
+    ],
+  },
+  {
+    question: " አንድ  በትራንስፖርት  ዘርፍ  የተሰማራ  ግለሰብ  ከቦታ  ወደ  ቦታ  በሚንቀሳቀስበት ጊዜ የአከባቢውን ህብረተሰብ       ጠንቅቆ  መረዳት  ሊገጥመው  የሚችለውን  ችግር  ያቃልላል፡፡ ",
+    answer: [
+      { text: "ቋንቋ ", correct: false },
+      { text: " ሀይማኖት ", correct: false },
+      { text: ". ባህል ", correct: false },
+      { text: "ሁሉም መልስ ነው", correct: true },
+    ],
+  },
+  {
+    question: " አንድ  በትራንስፖርት  ዘርፍ  የተሰማራ  ግለሰብ  ከቦታ  ወደ  ቦታ  በሚንቀሳቀስበት ጊዜ የአከባቢውን ህብረተሰብ       ጠንቅቆ  መረዳት  ሊገጥመው  የሚችለውን  ችግር  ያቃልላል፡፡ ",
+    answer: [
+      { text: "ቋንቋ ", correct: false },
+      { text: " ሀይማኖት ", correct: false },
+      { text: ". ባህል ", correct: false },
+      { text: "ሁሉም መልስ ነው", correct: true },
+    ],
+  },
+  {
+    question: " አንድ  በትራንስፖርት  ዘርፍ  የተሰማራ  ግለሰብ  ከቦታ  ወደ  ቦታ  በሚንቀሳቀስበት ጊዜ የአከባቢውን ህብረተሰብ       ጠንቅቆ  መረዳት  ሊገጥመው  የሚችለውን  ችግር  ያቃልላል፡፡ ",
+    answer: [
+      { text: "ቋንቋ ", correct: false },
+      { text: " ሀይማኖት ", correct: false },
+      { text: ". ባህል ", correct: false },
+      { text: "ሁሉም መልስ ነው", correct: true },
+    ],
+  },
+  {
+    question: " አንድ  በትራንስፖርት  ዘርፍ  የተሰማራ  ግለሰብ  ከቦታ  ወደ  ቦታ  በሚንቀሳቀስበት ጊዜ የአከባቢውን ህብረተሰብ       ጠንቅቆ  መረዳት  ሊገጥመው  የሚችለውን  ችግር  ያቃልላል፡፡ ",
+    answer: [
+      { text: "ቋንቋ ", correct: false },
+      { text: " ሀይማኖት ", correct: false },
+      { text: ". ባህል ", correct: false },
+      { text: "ሁሉም መልስ ነው", correct: true },
+    ],
+  },
+  {
+    question: " አንድ  በትራንስፖርት  ዘርፍ  የተሰማራ  ግለሰብ  ከቦታ  ወደ  ቦታ  በሚንቀሳቀስበት ጊዜ የአከባቢውን ህብረተሰብ       ጠንቅቆ  መረዳት  ሊገጥመው  የሚችለውን  ችግር  ያቃልላል፡፡ ",
+    answer: [
+      { text: "ቋንቋ ", correct: false },
+      { text: " ሀይማኖት ", correct: false },
+      { text: ". ባህል ", correct: false },
+      { text: "ሁሉም መልስ ነው", correct: true },
+    ],
+  },
+  {
+    question: " አንድ  በትራንስፖርት  ዘርፍ  የተሰማራ  ግለሰብ  ከቦታ  ወደ  ቦታ  በሚንቀሳቀስበት ጊዜ የአከባቢውን ህብረተሰብ       ጠንቅቆ  መረዳት  ሊገጥመው  የሚችለውን  ችግር  ያቃልላል፡፡ ",
+    answer: [
+      { text: "ቋንቋ ", correct: false },
+      { text: " ሀይማኖት ", correct: false },
+      { text: ". ባህል ", correct: false },
+      { text: "ሁሉም መልስ ነው", correct: true },
+    ],
+  },
+  {
+    question: " አንድ  በትራንስፖርት  ዘርፍ  የተሰማራ  ግለሰብ  ከቦታ  ወደ  ቦታ  በሚንቀሳቀስበት ጊዜ የአከባቢውን ህብረተሰብ       ጠንቅቆ  መረዳት  ሊገጥመው  የሚችለውን  ችግር  ያቃልላል፡፡ ",
+    answer: [
+      { text: "ቋንቋ ", correct: false },
+      { text: " ሀይማኖት ", correct: false },
+      { text: ". ባህል ", correct: false },
+      { text: "ሁሉም መልስ ነው", correct: true },
+    ],
+  },
+  {
+    question: " አንድ  በትራንስፖርት  ዘርፍ  የተሰማራ  ግለሰብ  ከቦታ  ወደ  ቦታ  በሚንቀሳቀስበት ጊዜ የአከባቢውን ህብረተሰብ       ጠንቅቆ  መረዳት  ሊገጥመው  የሚችለውን  ችግር  ያቃልላል፡፡ ",
+    answer: [
+      { text: "ቋንቋ ", correct: false },
+      { text: " ሀይማኖት ", correct: false },
+      { text: ". ባህል ", correct: false },
+      { text: "ሁሉም መልስ ነው", correct: true },
+    ],
+  },
+  {
+    question: " አንድ  በትራንስፖርት  ዘርፍ  የተሰማራ  ግለሰብ  ከቦታ  ወደ  ቦታ  በሚንቀሳቀስበት ጊዜ የአከባቢውን ህብረተሰብ       ጠንቅቆ  መረዳት  ሊገጥመው  የሚችለውን  ችግር  ያቃልላል፡፡ ",
+    answer: [
+      { text: "ቋንቋ ", correct: false },
+      { text: " ሀይማኖት ", correct: false },
+      { text: ". ባህል ", correct: false },
+      { text: "ሁሉም መልስ ነው", correct: true },
+    ],
+  },
+  {
+    question: " አንድ  በትራንስፖርት  ዘርፍ  የተሰማራ  ግለሰብ  ከቦታ  ወደ  ቦታ  በሚንቀሳቀስበት ጊዜ የአከባቢውን ህብረተሰብ       ጠንቅቆ  መረዳት  ሊገጥመው  የሚችለውን  ችግር  ያቃልላል፡፡ ",
+    answer: [
+      { text: "ቋንቋ ", correct: false },
+      { text: " ሀይማኖት ", correct: false },
+      { text: ". ባህል ", correct: false },
+      { text: "ሁሉም መልስ ነው", correct: true },
+    ],
+  },
+  {
+    question: " አንድ  በትራንስፖርት  ዘርፍ  የተሰማራ  ግለሰብ  ከቦታ  ወደ  ቦታ  በሚንቀሳቀስበት ጊዜ የአከባቢውን ህብረተሰብ       ጠንቅቆ  መረዳት  ሊገጥመው  የሚችለውን  ችግር  ያቃልላል፡፡ ",
+    answer: [
+      { text: "ቋንቋ ", correct: false },
+      { text: " ሀይማኖት ", correct: false },
+      { text: ". ባህል ", correct: false },
+      { text: "ሁሉም መልስ ነው", correct: true },
+    ],
+  },
+];
+const questionElement = document.getElementById("question");
+const answerBtns = document.getElementById("answer-buttons");
+const nextbtn = document.getElementById("next-btn");
+const timerElement = document.getElementById("timer");
+
+let currentQuestionIndex = 0;
+let score = 0;
+let initialTime = 10;
+let timeElapsed = 0;
+let timerInterval;
+
+function startTimer() {
+  timerInterval = setInterval(() => {
+    timeElapsed++;
+    updateTimerDisplay();
+    checkTimeLimit();
+  }, 1000);
+}
+function updateTimerDisplay() {
+  const remainingTime = initialTime - timeElapsed;
+  const minutes = Math.floor(remainingTime / 60);
+  const seconds = remainingTime % 60;
+  const formattedTime = 
+  `${minutes}:${seconds.toString().padStart(2, '0')}`;
+  timerElement.textContent = formattedTime;
+}
+
+function checkTimeLimit() {
+  if (timeElapsed >= initialTime) {
+    clearInterval(timerInterval);
+    showScore();
+  }
+}
+
+function startQuiz() {
+  initialTime = 30;
+  timeElapsed = 0;
+  startTimer();
+
+  currentQuestionIndex = 0;
+  score = 0;
+  nextbtn.innerHTML = "ቀጣይ";
+  showQuestion();
+}
+
+function showQuestion() {
+  resetState();
+  let currentQuestion = questions[currentQuestionIndex];
+  let questionNO = currentQuestionIndex + 1;
+  questionElement.innerHTML = 
+  questionNO + ". " + currentQuestion.question;
+
+  currentQuestion.answer.forEach(answer => {
+    const button = document.createElement("button");
+    button.innerHTML = answer.text;
+    button.classList.add("btn");
+    answerBtns.appendChild(button);
+    if (answer.correct) {
+      button.dataset.correct = answer.correct;
+    }
+    button.addEventListener("click", selectAnswer);
+  });
+}
+
+function resetState() {
+  nextbtn.style.display = "none";
+  while (answerBtns.firstChild) {
+    answerBtns.removeChild(answerBtns.firstChild);
+  }
+}
+
+function selectAnswer(e) {
+  const selectedBtn = e.target;
+  const isCorrect = selectedBtn.dataset.correct === "true";
+  if (isCorrect) {
+    selectedBtn.classList.add("correct");
+    score++;
+  } else {
+    selectedBtn.classList.add("incorrect");
+  }
+  Array.from(answerBtns.children).forEach(button => {
+    if (button.dataset.correct === "true") {
+      button.classList.add("correct");
+    }
+    button.disabled = true;
+  });
+  nextbtn.style.display = "block";
+}
+
+function showScore() {
+  clearInterval(timerInterval); // Stop the timer
+  resetState();
+  questionElement.innerHTML =
+   `You scored ${score} out of ${questions.length}!`;
+  nextbtn.innerHTML = "እንደገና ይሞክሩ";
+  nextbtn.style.display = "block";//to show hidden HTML element
+}
+
+function handleNextButton() {
+  currentQuestionIndex++;
+  if (currentQuestionIndex < questions.length) {
+    showQuestion();
+  } else {
+    showScore();
+    nextbtn.innerHTML = "እንደገና ይሞክሩ";
+  }
+}
+
+nextbtn.addEventListener("click", () => {
+  if (nextbtn.innerHTML === "እንደገና ይሞክሩ") {
+    startQuiz();
+  } else {
+    handleNextButton(); 
+  }
+});
+
+startQuiz();
